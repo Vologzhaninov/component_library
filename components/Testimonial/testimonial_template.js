d9ad091ca04fe5d78fe0123d11c7dc3e7d8922ae
Vologzhaninov <d.vologzhaninov@gmail.com>
2026-02-22 22:44:01 -0800

    image fix

--- components/Testimonial/testimonial_template.js
+++ components/Testimonial/testimonial_template.js
@@ -4,6 +4,6 @@ export default {
     position: "CTO",
     text1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit.",
     text2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.",
-    image: "/images/image.jpg",
-    logo: "/images/logo.png"
+    image: "./images/image.jpg",
+    logo: "./images/logo.png"
 }
